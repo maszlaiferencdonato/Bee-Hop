5e95fb4c9dd6ea5b6a398e9be412f4ea766264d3
parasztkolbasz <epick.gamer6900@gmail.com>
2026-09-22 12:22:50 +0200

    Add files via upload

--- bee.js
+++ bee.js
@@ -1,3 +1,55 @@
+let oszlopok = [];
+const jatekTer = document.getElementById('jatekTer');
+const oszlopSzelesseg = 60;
+const resMeret = 180;
+
+function valtsTemat(ujTema) {
+    jatekTer.className = 'jatek-ter ' + ujTema;
+}
+
+function hozzaadOszlop() {
+    const minMagassag = 50;
+    const maxMagassag = 750 - 80 - resMeret - minMagassag;
+    const felsoMagassag = Math.floor(Math.random() * (maxMagassag - minMagassag + 1)) + minMagassag;
+    const alsoMagassag = 750 - 80 - felsoMagassag - resMeret;
+
+    const felsoOszlop = document.createElement('div');
+    felsoOszlop.className = 'oszlop';
+    felsoOszlop.style.height = felsoMagassag + 'px';
+    felsoOszlop.style.top = '0px';
+    felsoOszlop.style.left = '600px';
+
+    const alsoOszlop = document.createElement('div');
+    alsoOszlop.className = 'oszlop';
+    alsoOszlop.style.height = alsoMagassag + 'px';
+    alsoOszlop.style.bottom = '80px';
+    alsoOszlop.style.left = '600px';
+
+    jatekTer.appendChild(felsoOszlop);
+    jatekTer.appendChild(alsoOszlop);
+
+    oszlopok.push({ felso: felsoOszlop, also: alsoOszlop, x: 600 });
+}
+
+function mozgatas() {
+    for (let i = 0; i < oszlopok.length; i++) {
+        let o = oszlopok[i];
+        o.x -= 2;
+        o.felso.style.left = o.x + 'px';
+        o.also.style.left = o.x + 'px';
+
+        if (o.x < -oszlopSzelesseg) {
+            o.felso.remove();
+            o.also.remove();
+            oszlopok.splice(i, 1);
+            i--;
+        }
+    }
+}
+
+setInterval(hozzaadOszlop, 2500);
+setInterval(mozgatas, 20);
+
 function valtsTemat(ujTema) {
     const jatekTer = document.getElementById('jatekTer');
     jatekTer.className = 'jatek-ter ' + ujTema;
@@ -10,19 +62,6 @@ const ctx = canvas.getContext("2d");
 
 const playerImg = new Image();
 playerImg.src = "mehecske.png";
-    
-   
-    jatekTer.className = 'jatek-ter ' + ujTema;
-}
-
-   
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
-
-       
-
-const playerImg = new Image();
-
 let playerImgLoaded = false;
 playerImg.onload = () => { playerImgLoaded = true; };
 
@@ -59,13 +98,6 @@ function update() {
         bird.y = 0;
         bird.velocity = 0;
     }
-    velocity: 0
-};
-
-function update() {
-    
-    bird.velocity += bird.gravity;
-    bird.y += bird.velocity;
 }
 
 function draw() {
@@ -78,6 +110,8 @@ function draw() {
         ctx.fillRect(bird.x, bird.y, bird.width, bird.height);
     }
 }
+
+  
 
 function loop() {
     update();
